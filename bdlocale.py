c13b7fbed72c3c18a919e3648db15bf0422026a8
Émile Jetzer <emile.jetzer@polymtl.ca>
2020-11-24 03:24:18 -0500

    Fusion des programmes en une classe de gestion.

--- bdlocale.py
+++ bdlocale.py
@@ -7,69 +7,130 @@ Created on Sat Nov 21 16:14:41 2020
 """
 
 import argparse
-#import time
 import configparser
-#import asyncio
 from typing import Union, Sequence
+#import sqlite3 as db
 
-#import paramiko #  Protocole SSH
-import sqlite3 as db
 #import MySQLdb as db
 import pandas as pd
+import paramiko
 
-class BaseDeDonnées:
+class Gestionnaire:
 
-    def __init__(self):
+    def __init__(self, dbmodule='sqlite3'):
+        self.db = importlib.import_module(dbmodule)
         self.args = argparse.ArgumentParser(description='Gérer la base de données')
+        self.config = configparser.ConfigParser()
+    
+    def définir_args(self):
         self.args.add_argument('--config',
                              type=str, default='heures.config',
                              dest='config',
                              help='Fichier de configuration à utiliser')
-        self.config = configparser.ConfigParser()
-
+        
+        self.sub_args = self.args.add_subparsers()
+        
+        self.db_args = self.sub_args.add_parser('db')
+        self.db_args.add_argument('-h', '--heure')
+        self.db_args.add_argument('-d', '--description')
+        self.db_args.add_argument('-m', '--montage')
+        self.db_args.add_argument('-r', '--responsable')
+        self.db_args.add_argument('-a', '--atelier')
+        
+        self.ssh_args = self.sub_args.add_parser('ssh')
+        self.ssh_args.add_argument('--url')
+        self.ssh_args.add_argument('-h', '--heure')
+        self.ssh_args.add_argument('-d', '--description')
+        self.ssh_args.add_argument('-m', '--montage')
+        self.ssh_args.add_argument('-r', '--responsable')
+        self.ssh_args.add_argument('-a', '--atelier')
+    
     def parse_args(self):
-        arguments = self.args.parse_args()
-        fichier_config = arguments.config
+        self.arguments = self.args.parse_args()
+        fichier_config = self.arguments.config
         self.config.read(fichier_config)
 
-        return arguments
+        return self.arguments
+    
+    def db_main(self):
+        pass
+    
+    def ssh_main(self):
+        pass
+    
+    def __run__(self):
+        pass
+    
+    def ssh_connect(self):
+        self.ssh_client = paramiko.SSHClient()
+        self.ssh_client.load_system_host_keys()
+        self.ssh_client.connect(self.config['ssh']['serveur'])
+    
+    def ssh_attendre(self,
+                     canal: paramiko.channel.Channel,
+                     attente: float = 1,
+                     buf: int = 1024):
+        sortie = b''
+        while not (canal.recv_ready() and canal.send_ready()):
+            if canal.recv_ready():
+                sortie += canal.recv(buf)
+            else:
+                time.sleep(attente)
 
-    def connect(self):
+        return str(sortie, encoding='utf-8')
+    
+    def ssh_invoke_shell(self):
+        self.canal = self.ssh_client.invoke_shell()
+        return self.canal
+    
+    def ssh_close(self):
+        self.ssh_client.close()
+    
+    def ssh_exécuter(self,
+                     commande: str,
+                     canal: paramiko.channel.Channel,
+                     attente: float = 1):
+        if not commande.endswith('\n'):
+            commande += '\n'
+        canal.send(commande)
+        return attendre(canal, attente)
+
+    def db_connect(self):
         url_database = self.config['mysql']['bd']
-        self.conn = db.connect(url_database)
+        self.conn = self.db.connect(url_database)
 
-    def réinitialiser(self):
+    def db_réinitialiser(self):
         script_réinit = self.config['mysql']['init']
         with open(script_réinit) as fichier_réinit:
-            self.executescript(fichier_réinit.read())
+            self.db_executescript(fichier_réinit.read())
 
-    def cursor(self, factory=db.Cursor):
+    def db_cursor(self, factory=db.Cursor):
         return self.conn.cursor(factory)
 
-    def execute(self, sql: str, parameters: tuple = tuple(), *args, **kargs):
+    def db_execute(self, sql: str, parameters: tuple = tuple(), *args, **kargs):
         rés = self.conn.execute(sql, parameters, *args, **kargs)
-        self.commit()
+        self.db_commit()
         return rés
 
-    def executemany(self, sql: str,
+    def db_executemany(self, sql: str,
                     seq_of_parameters: Sequence[Union[Sequence, dict]] = tuple(),
                     *args, **kargs):
         rés = self.conn.executemany(sql, seq_of_parameters, *args, **kargs)
-        self.commit()
+        self.db_commit()
         return rés
 
-    def executescript(self, sql_script: str, *args, **kargs):
+    def db_executescript(self, sql_script: str, *args, **kargs):
         rés = self.conn.executescript(sql_script, *args, **kargs)
-        self.commit()
+        self.db_commit()
         return rés
 
-    def commit(self):
+    def db_commit(self):
         self.conn.commit()
 
-    def close(self):
+    def db_close(self):
         self.conn.close()
 
-    def select(self, cols: Union[Sequence, str], table: str,
+    def db_select(self, cols: Union[Sequence, str], table: str,
                lim: int = 1000, chunksize: int = 20):
         if not isinstance(cols, str):
             cols = '{}'.format(', '.join(f'`{c}`' for c in cols))
@@ -79,7 +140,7 @@ class BaseDeDonnées:
                                        index_col='id', chunksize=chunksize):
             yield chunk
 
-    def insert(self, table: str, **data):
+    def db_insert(self, table: str, **data):
         cols = '({})'.format(', '.join(f'`{c}`' for c in data.keys()))
         vals = '({})'.format(', '.join(f':{c}' for c in data.keys()))
         requête = f'insert into {table} {cols} values {vals}'
@@ -88,9 +149,9 @@ class BaseDeDonnées:
         vals = data.values()
         gen = ({c: v for c, v in zip(clés, vs)} for vs in zip(*vals))
 
-        self.executemany(requête, gen)
+        self.db_executemany(requête, gen)
 
-    def update(self, table: str, where: str, **data):
+    def db_update(self, table: str, where: str, **data):
         cols = ', '.join(f'{c}=:{c}' for c in data.keys() if not c.startswith('where'))
         requête = f'update {table} set {cols} where {where}'
 
@@ -104,10 +165,10 @@ class BaseDeDonnées:
         #for d in gen:
         #    self.execute(requête, d)
 
-        self.executemany(requête, gen)
+        self.db_executemany(requête, gen)
 
     def groupes(self):
-        yield from self.select('*', 'groupes')
+        yield from self.db_select('*', 'groupes')
 
     def nouveau_groupe(self, nom: Union[str, Sequence],
                        responsable: Union[str, Sequence] = None,
@@ -124,10 +185,10 @@ class BaseDeDonnées:
         while len(nom) > len(description):
             description.append(None)
 
-        self.insert('groupes', nom=nom, responsable=responsable, description=description)
+        self.db_insert('groupes', nom=nom, responsable=responsable, description=description)
 
     def labos(self):
-        yield from self.select('*', 'laboratoires')
+        yield from self.db_select('*', 'laboratoires')
 
     def nouveau_labo(self, local: Union[str, Sequence],
                      groupe: Union[str, Sequence],
@@ -144,7 +205,7 @@ class BaseDeDonnées:
                 if g in g_df.nom.values:
                     groupe_existe[i] = True
         if not all(groupe_existe):
-            raise db.DataError(f"Le groupe '{groupe}' n'existe pas.")
+            raise self.db.DataError(f"Le groupe '{groupe}' n'existe pas.")
 
         if not isinstance(responsable, Sequence) or isinstance(responsable, str):
             responsable = [responsable]
@@ -152,7 +213,7 @@ class BaseDeDonnées:
         while len(local) > len(responsable):
             responsable.append(None)
 
-        self.insert('laboratoires', local=local, groupe=groupe, responsable=responsable)
+        self.db_insert('laboratoires', local=local, groupe=groupe, responsable=responsable)
 
     def montages(self):
         yield from self.select('*', 'montages')
@@ -177,7 +238,7 @@ class BaseDeDonnées:
                 if g in g_df.nom.values:
                     groupe_existe[i] = True
         if not all(groupe_existe):
-            raise db.DataError(f"Le groupe {groupe} n'existe pas.")
+            raise self.db.DataError(f"Le groupe {groupe} n'existe pas.")
 
         labo_existe = [False for i in laboratoire]
         for l_df in self.labos():
@@ -185,7 +246,7 @@ class BaseDeDonnées:
                 if l in l_df.local.values:
                     labo_existe[i] = True
         if not all(labo_existe):
-            raise db.DataError(f"Le laboratoire {laboratoire} n'existe pas.")
+            raise self.db.DataError(f"Le laboratoire {laboratoire} n'existe pas.")
 
         if not isinstance(responsable, Sequence) or isinstance(responsable, str):
             responsable = [responsable]
@@ -197,10 +258,10 @@ class BaseDeDonnées:
         while len(nom) > len(description):
             description.append(None)
 
-        self.insert('montages', nom=nom, groupe=groupe, laboratoire=laboratoire, responsable=responsable, description=description)
+        self.db_insert('montages', nom=nom, groupe=groupe, laboratoire=laboratoire, responsable=responsable, description=description)
 
     def projets(self):
-        yield from self.select('*', 'projets')
+        yield from self.db_select('*', 'projets')
 
     def nouveau_projet(self, nom: Union[str, Sequence],
                        montage: Union[str, Sequence],
@@ -230,10 +291,10 @@ class BaseDeDonnées:
         while len(nom) > len(description):
             description.append(None)
 
-        self.insert('projets', nom=nom, montage=montage, responsable=responsable, description=description)
+        self.db_insert('projets', nom=nom, montage=montage, responsable=responsable, description=description)
 
     def taches(self):
-        yield from self.select('*', 'taches')
+        yield from self.db_select('*', 'taches')
 
     def nouvelle_tache(self, description: Union[str, Sequence],
                        projet: Union[str, Sequence],
@@ -256,7 +317,7 @@ class BaseDeDonnées:
                 if p in p_df.nom.values:
                     projet_existe[i] = True
         if not all(projet_existe):
-            raise db.DataError(f"Le projet {projet} n'existe pas.")
+            raise self.db.DataError(f"Le projet {projet} n'existe pas.")
 
         parent_existe = [(i is None) for i in parent]
         for p_df in self.taches():
@@ -264,7 +325,7 @@ class BaseDeDonnées:
                 if (p in p_df.nom.values) or (p is None):
                     parent_existe[i] = True
         if not all(parent_existe):
-            raise db.DataError(f"Le parent {parent} n'existe pas.")
+            raise self.db.DataError(f"Le parent {parent} n'existe pas.")
 
         if not isinstance(temps, Sequence):
             temps = [temps]
@@ -276,7 +337,7 @@ class BaseDeDonnées:
         while len(fini) > len(description):
             fini.append(False)
 
-        self.insert('taches', description=description, projet=projet, parent=parent, temps=temps, fini=fini)
+        self.db_insert('taches', description=description, projet=projet, parent=parent, temps=temps, fini=fini)
 
     def maj_tache(self, description: Union[str, Sequence],
                   temps: Union[float, Sequence],
@@ -293,7 +354,7 @@ class BaseDeDonnées:
         while len(description) > len(fini):
             fini.append(False)
 
-        t_df = pd.concat(self.select(('id', 'description', 'temps'), 'taches'))
+        t_df = pd.concat(self.db_select(('id', 'description', 'temps'), 'taches'))
         t_df_indices = t_df.description == description
         t_df = t_df[t_df_indices]
         description_classé, temps_classé, fini_classé = [], [], []
@@ -304,11 +365,11 @@ class BaseDeDonnées:
             temps_classé.append(temps[index] + t)
             fini_classé.append(fini[index])
 
-        self.update('taches', 'description=:where_description', temps=temps_classé, fini=fini_classé, where_description=description_classé)
-        self.commit()
+        self.db_update('taches', 'description=:where_description', temps=temps_classé, fini=fini_classé, where_description=description_classé)
+        self.db_commit()
 
     def __enter__(self):
-        self.connect()
+        self.db_connect()
         return self
 
     def __exit__(self, type, value, traceback):
@@ -318,43 +379,15 @@ def main():
     base_de_données = BaseDeDonnées()
     base_de_données.parse_args()
     with base_de_données:
-        base_de_données.réinitialiser()
+        base_de_données.db_réinitialiser()
 
         base_de_données.nouveau_groupe('femtoQ', 'Denis Seletskiy')
-
-        for g in base_de_données.groupes():
-            pass#print(g)
-
         base_de_données.nouveau_labo('B-585', 'femtoQ', 'Denis Seletskiy')
-
-        for l in base_de_données.labos():
-            pass#print(l)
-
         base_de_données.nouveau_montage('SuperContinuum', 'femtoQ', 'B-585', 'Denis Seletskiy')
-
-        for m in base_de_données.montages():
-            pass#print(m)
-
         base_de_données.nouveau_projet('Projet test', 'SuperContinuum', 'Étienne Doiron')
-
-        for p in base_de_données.projets():
-            pass#print(p)
-
         base_de_données.nouvelle_tache('Tâche test', 'Projet test')
-
-        for t in base_de_données.taches():
-            print(t)
-
         base_de_données.maj_tache('Tâche test', 1, True)
-
-        for t in base_de_données.taches():
-            print(t)
-
         base_de_données.maj_tache('Tâche test', 2, False)
-
-        for t in base_de_données.taches():
-            print(t)
-
         base_de_données.maj_tache('Tâche test', 1, True)
 
         for t in base_de_données.taches():
