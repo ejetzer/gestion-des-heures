c501924aca803942c33ef66e73f651f97e6f3a3c
Émile Jetzer <emile.jetzer@gmail.com>
2021-04-13 08:54:43 -0400

    Modification pour permettre le formatage de données entre les analyses automatisées

--- script_atelier.py
+++ script_atelier.py
@@ -9,12 +9,16 @@ Created on Wed Mar  3 10:28:33 2021
 import pandas as pd
 from pathlib import Path
 from datetime import datetime as Dt
-#import shutil
+import shutil
 #import itertools
 
 
+racine = Path.home() / 'Library' / 'Mobile Documents' / \
+    'iCloud~is~workflow~my~workflows' / 'Documents'
+boite_de_dépôt = racine / 'Journal de bord/'
 activités = Path.home() / 'OneDrive - polymtl.ca' / 'Heures'
 fichier = 'résumé *-*-* *_*.xlsx'
+archive = boite_de_dépôt / 'Heures comptées'
 moment = Dt.now()
 
 df = pd.concat([pd.read_excel(f, 0) for f in activités.glob(fichier)],
@@ -26,12 +30,14 @@ atelier = df.loc[df.Atelier.astype(bool), ['Payeur',
                                            'Description des travaux effectués',
                                            'Nbr d\'heures ']]
 minutes = df.loc[df.Atelier.astype(bool), 'Temps (min)'] / 60.0
-atelier['Nbr d\'heures '] = atelier['Nbr d\'heures '].str\
-                                                     .replace(',', '.')\
-                                                     .astype(float)\
-                                                     .fillna(minutes)\
+atelier['Nbr d\'heures '] = atelier['Nbr d\'heures '].fillna(minutes)\
                                                      .multiply(1.5)
 
 with pd.ExcelWriter(activités / f'atelier {moment:%Y-%m-%d %H_%M}.xlsx') as excel:
     for groupe, heures in atelier.groupby('Payeur'):
         heures.to_excel(excel, sheet_name=groupe)
+
+# Archiver les fichiers
+for f in activités.glob(fichier):
+    print(f'Déplacer {f} vers {archive}...')
+    shutil.move(f, archive)
